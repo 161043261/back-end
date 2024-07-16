2a6327e7833520d12b6fe2f63b39e00526ac5495
Tiancheng <yukino161043261@gmail.com>
2024-07-16 19:00:02 +0800

    Initial commit

---
+++ web/servletDemo/src/main/java/com/bronya/servletDemo/servlets/Attribute.java
@@ -0,0 +1,21 @@
+package com.bronya.servletDemo.servlets;
+
+import javax.servlet.ServletContext;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/attribute")
+public class Attribute extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) {
+        ServletContext context = getServletContext();
+        context.setAttribute("username", "root"); // insert
+        context.setAttribute("password", "0000"); // insert
+        context.setAttribute("password", "1024"); // update
+        String username = (String) context.getAttribute("username"); // select
+        System.out.println(username);
+        context.removeAttribute("password");
+    }
+}
---
+++ web/servletDemo/src/main/java/com/bronya/servletDemo/servlets/HttpServletRequestTest.java
@@ -0,0 +1,73 @@
+package com.bronya.servletDemo.servlets;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.Map;
+
+/**
+ * req.getMethod();
+ * req.getRequestURI();
+ * req.getRequestURL();
+ * req.getScheme();
+ * req.getProtocol();
+ * req.getLocalPort();
+ * req.getServerPort();
+ * req.getRemotePort();
+ * req.getHeader();
+ * req.getHeaderNames();
+ * req.getParameter();
+ * req.getParameter();
+ * req.getParameterNames();
+ * req.getParameterMap();
+ * req.getReader();
+ * req.getInputStream();
+ * req.getServletPath()
+ */
+@WebServlet("/req")
+public class HttpServletRequestTest extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) {
+        // Request Line
+        System.out.println("********** Request Line ********** ");
+        System.out.println(req.getMethod());     // GET/POST
+        System.out.println(req.getRequestURI()); // /req
+        System.out.println(req.getRequestURL()); // http://127.0.0.1:8080/req
+        System.out.println(req.getScheme());     // http
+        System.out.println(req.getProtocol());   // HTTP/1.1
+        // Remote Browser => Proxy Server => Local Tomcat
+        System.out.println(req.getLocalPort());  // Local Tomcat Port | 8080
+        System.out.println(req.getServerPort()); // Proxy Server Port | 8080
+        System.out.println(req.getRemotePort()); // Remote Browser Port
+
+        // Request Headers
+        System.out.println("********** Request Headers **********"); // Name: Value
+        System.out.println("accept: " + req.getHeader("accept"));
+        Enumeration<String> headerNames = req.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String name = headerNames.nextElement();
+            String value = req.getHeader(name);
+            System.out.println(name + ": " + value);
+        }
+
+        // Request K-V Parameters (Request Line & Request Body)
+        System.out.println("********** Request K-V Parameters (Request Line & Request Body)  **********");
+        String username = req.getParameter("username");
+        System.out.println("username: " + username);
+        Enumeration<String> parameterNames = req.getParameterNames();
+        while (parameterNames.hasMoreElements()) {
+            String name = parameterNames.nextElement();
+            String[] values = req.getParameterValues(name);
+            System.out.println(name + ": " + Arrays.toString(values));
+        }
+        Map<String, String[]> map = req.getParameterMap();
+        map.forEach((k, v) -> System.out.println(k + ": " + Arrays.toString(v)));
+
+        // BufferedReader reader = req.getReader(); // character input stream
+        // ServletInputStream stream = req.getInputStream(); // byte input stream
+        System.out.println(req.getServletPath()); // /req
+    }
+}
