2c8e2a5e0f2bd3bf24788ba83e7bd30172ce5fce
Tiancheng <yukino161043261@gmail.com>
2024-07-19 18:03:08 +0800

    Initial commit

--- go-dev/orm/build.sh
+++ go-dev/orm/build.sh
@@ -1,3 +1,5 @@
 go mod init bronya.com/hello
 
-go 
+go get -u gorm.io/gorm
+go get -u gorm.io/driver/sqlite
+go get -i gorm.io/driver/mysql
--- web/mybatis-demo/src/main/java/com/bronya/mybatisdemo/mapper/UserMapper.java
+++ web/mybatis-demo/src/main/java/com/bronya/mybatisdemo/mapper/UserMapper.java
@@ -2,6 +2,16 @@ package com.bronya.mybatisdemo.mapper;
 
 import com.bronya.mybatisdemo.pojo.User;
 
+import java.util.List;
+
 public interface UserMapper {
     int insertUser();
+
+    int updateUser();
+
+    int deleteUser();
+
+    User getUserById();
+
+    List<User> getUsers();
 }
